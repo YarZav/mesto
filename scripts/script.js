5893b1a623e4f47728493100887590354a076478
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-03-04 12:04:11 +0100

    Исправил замечания по код ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,112 +1,45 @@
-// Start state
-let profileNameText = "Жак-Ив Кусто";
-let profileDescriptionText = "Исследователь океана";
-let isPopupDisplayed = false;
+// Properties
+let body = document.querySelector("body");
 
-setDefaultProfileNameAndDescription();
+let popup = body.querySelector(".popup");
+let popupContainer = popup.querySelector(".popup__container");
+let popupCrossButton = popupContainer.querySelector(".popup__cross-button");
+let popupSubmitButton = popupContainer.querySelector(".popup__submit-button");
+let popupName = popupContainer.querySelector(".popup__input__name");
+let popupDescription = popupContainer.querySelector(".popup__input__description");
+
+let profile = body.querySelector(".profile");
+let profileInfo = profile.querySelector(".profile__info");
+let profileEdit = profileInfo.querySelector(".profile__edit");
+let profileName = profileEdit.querySelector(".profile__name");
+let profileEditButton = profileEdit.querySelector(".profile__edit-button");
+let profileDescription = profileInfo.querySelector(".profile__description");
+
+popupCrossButton.addEventListener("click", onClosePopup)
+popupSubmitButton.addEventListener("click", onSubmitPopup)
+profileEditButton.addEventListener("click", onShowPopup)
 
 // Name and description
 
-function onSubmit() {
-    let popupName = getPopupName();
-    let popupDescription = getPopupDescription();
-
-    let profileName = getProfileName();
-    let profileDescription = getProfileDescription();
-
+function onSubmitPopup() {
     profileName.textContent = popupName.value;
     profileDescription.textContent = popupDescription.value;
 
-    isPopupDisplayed = false;
-    setPopupDisplay();
-}
-
-function setDefaultProfileNameAndDescription() {
-    let profileName = getProfileName();
-    let profileDescription = getProfileDescription();
-
-    profileName.textContent = profileNameText;
-    profileDescription.textContent = profileDescriptionText;    
-}
-
-function getProfileName() {
-    let profileInfo = getProfileInfo();
-    let profileEdit = profileInfo.querySelector(".profile__edit");
-    let profileName = profileEdit.querySelector(".profile__name"); 
-    return profileName;
-}
-
-function getProfileDescription() {
-    let profileInfo = getProfileInfo();
-    let profileDescription = profileInfo.querySelector(".profile__description");
-    return profileDescription;
-}
-
-function getProfileInfo() {
-    let body = getBody();
-    let profile = body.querySelector(".profile");
-    let profileInfo = profile.querySelector(".profile__info");
-    return profileInfo
+    onClosePopup();
 }
 
 // Popup
 
-function onTogglePopup() {
-    isPopupDisplayed = !isPopupDisplayed;
-    setPopupDisplay();
-
-    if (isPopupDisplayed) {
-        setDefaulPopupNameAndDescription();
-    }
+function onShowPopup() {
+    popup.classList.add("popup_opened");
+    setDefaulPopupNameAndDescription();
 }
 
-function setPopupDisplay() {
-    let popup = getPopup();
-    if (isPopupDisplayed) {
-        popup.classList.add("popup_opened");
-    } else {
-        popup.classList.remove("popup_opened");
-    }
+function onClosePopup() {
+    popup.classList.remove("popup_opened");
 }
 
 function setDefaulPopupNameAndDescription() {
-    let popupName = getPopupName();
-    let popupDescription = getPopupDescription();
-
-    let profileName = getProfileName();
-    let profileDescription = getProfileDescription();
-
     popupName.value = profileName.textContent;
     popupDescription.value = profileDescription.textContent;
-}
-
-function getPopup() {
-    let body = getBody();
-    let popup = body.querySelector(".popup");
-    return popup;
-}
-
-function getPopupContainer() {
-    let popup = getPopup();
-    let popupContainer = popup.querySelector(".popup__container");
-    return popupContainer;
-}
-
-function getPopupName() {
-    let popupContainer = getPopupContainer();
-    let popupName = popupContainer.querySelector(".popup__input_name");
-    return popupName;
-}
-
-function getPopupDescription() {
-    let popupContainer = getPopupContainer();
-    let popupDescription = popupContainer.querySelector(".popup__input_description");
-    return popupDescription;
-}
-
-// Main body
-
-function getBody() {
-    let body = document.querySelector("body");
-    return body;
 }
