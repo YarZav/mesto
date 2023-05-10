3a459af9efeb22187a7b2a92d6e2d4d99c718b09
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-05-10 19:57:28 +0200

    Удалил исcкуственное генерирование события ввода input

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -43,14 +43,6 @@ class PopupWithForm extends Popup {
         this._popup.addEventListener("submit", this._handleSubmitBinder);
     }
 
-    open() {
-        super.open();
-
-        this._inputList.forEach((input) => {
-            input.dispatchEvent(new Event('keyup'));
-        });
-    }
-
     close() {
         this._reset();
 
